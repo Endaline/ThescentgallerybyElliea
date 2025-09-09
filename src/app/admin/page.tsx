e3fc734973791dd92f6d10962198a763036ec58f
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2025-09-09 05:09:45 +0100

    chore: added auth-guard to admin pages

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AdminDashboard from "./_components/dashboard";
 import { getOrderSummary } from "@/app/actions/order.actions";
+import { requireAdmin } from "@/services/auth-guard";
 
 const page = async () => {
   const summary = await getOrderSummary();
   console.log(summary);
+  await requireAdmin();
   return <AdminDashboard />;
 };
 
